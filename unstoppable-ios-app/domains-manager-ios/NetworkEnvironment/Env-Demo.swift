dd2941bfd41976be7ba3461df91c832c07bbcfe6
rommex <roman@unstoppabledomains.com>
2023-01-23 16:17:15 +0200

    comments for Env file -- keys and IDs

--- unstoppable-ios-app/domains-manager-ios/NetworkEnvironment/Env-Demo.swift
+++ unstoppable-ios-app/domains-manager-ios/NetworkEnvironment/Env-Demo.swift
@@ -8,27 +8,35 @@
 import Foundation
 
 // This is how the file GateConfiguration.swift is done:
-// If you compile your code, please use your own project ID
+// If you compile your code, please use your own project IDs
 
 extension NetworkService {
+    
+    // Your Infura Project ID as an JRPC Provider
     static let mainnetInfuraProjectId = "<your_project_id>"
     static let rinkebyInfuraProjectId = "<your_project_id>"
     
+    // Internal UD API
     static let mainnetMetadataAPIKey = "<mainnet-api-key>"
     static let testnetMetadataAPIKey = "<testnet-api-key>"
     
+    // Analytics API, you may keep these empty
     static let heapProdAppId = "prod-heap-key"
     static let heapDevAppId = "dev-heap-key"
     
+    // Needed for push notifications in WalletConnect2
     static let wc2EchoServerProdHost = "https://..."
     static let wc2EchoServerDevHost = "https://..."
 }
 
 struct AppIdentificators {
+    // WalletConnect2 Project Id
     static let wc2ProjectId = "<id>"
 }
 
 extension PaymentConfiguration {
+    // Need to enter keys to enable Apple Pay via Stripe
+    
     struct Merchant {
         static let identifier = "<merchant-id>"
     }
