9166d1602520524cdc93a9f5564ed927d9645311
Oleg Kuplin <>
2023-02-15 15:11:14 +0700

    Adjusted functions that control view's lifecycle within CNavigationController with push/pop other view controllers

--- unstoppable-ios-app/domains-manager-ios/Modules/CNavigation/CNavigationController.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/CNavigation/CNavigationController.swift
@@ -288,7 +288,8 @@ private extension CNavigationController {
         addChild(viewController)
         viewController.view.translatesAutoresizingMaskIntoConstraints = true
         viewController.view.frame = containerView.bounds
-        topViewController?.viewWillDisappear(animated)
+        topViewController?.beginAppearanceTransition(false, animated: animated)
+        viewController.beginAppearanceTransition(true, animated: animated)
         containerView.addSubview(viewController.view)
         delegate?.navigationController(self, willShow: viewController, animated: animated)
         
@@ -324,16 +325,22 @@ private extension CNavigationController {
             
             func finishTransition() {
                 transition.animationEnded?(true)
-                topViewController.viewDidDisappear(animated)
+                topViewController.endAppearanceTransition()
                 finishPush()
                 navigationBar.setBackButton(hidden: false)
-                viewController.viewDidAppear(animated)
+                viewController.endAppearanceTransition()
                 delegate?.navigationController(self, didShow: viewController, animated: animated)
             }
             
             func cancelTransition() {
+                viewController.beginAppearanceTransition(false, animated: animated)
                 viewController.view.removeFromSuperview()
+                viewController.endAppearanceTransition()
                 viewController.didMove(toParent: nil)
+                
+                topViewController.beginAppearanceTransition(true, animated: animated)
+                topViewController.endAppearanceTransition()
+                
                 setTransitioning(false)
             }
             
@@ -361,8 +368,8 @@ private extension CNavigationController {
         setTransitioning(true)
         let containerView = self.viewControllersContainerView!
         fromViewController.willMove(toParent: nil)
-        fromViewController.viewWillDisappear(animated)
-        toViewController.viewWillAppear(animated)
+        fromViewController.beginAppearanceTransition(false, animated: animated)
+        toViewController.beginAppearanceTransition(true, animated: animated)
         delegate?.navigationController(self, willShow: toViewController, animated: animated)
 
         let transition = transitionHandler.navigationController(self,
@@ -386,11 +393,11 @@ private extension CNavigationController {
         
         func finishTransition() {
             transition.animationEnded?(true)
-            fromViewController.viewDidDisappear(animated)
             fromViewController.view.removeFromSuperview()
+            fromViewController.endAppearanceTransition()
             fromViewController.removeFromParent()
             completion()
-            toViewController.viewDidAppear(animated)
+            toViewController.endAppearanceTransition()
             delegate?.navigationController(self, didShow: toViewController, animated: animated)
 
             if viewControllers.count == 1 {
@@ -401,8 +408,13 @@ private extension CNavigationController {
         }
         
         func cancelTransition() {
+            toViewController.beginAppearanceTransition(false, animated: animated)
+            toViewController.endAppearanceTransition()
+
+            fromViewController.beginAppearanceTransition(true, animated: animated)
+            fromViewController.endAppearanceTransition()
             fromViewController.didMove(toParent: self)
-            fromViewController.viewDidAppear(animated)
+            
             setTransitioning(false)
         }
         var isSwipe = false
