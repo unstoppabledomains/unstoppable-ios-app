bcf670d392279003a7eb80f03fe8084eeef5b50d
rommex <roman@unstoppabledomains.com>
2023-03-10 12:12:26 +0200

    refact

--- unstoppable-ios-app/domains-manager-ios/Entities/Domains/DomainItem.swift
+++ unstoppable-ios-app/domains-manager-ios/Entities/Domains/DomainItem.swift
@@ -92,14 +92,6 @@ extension DomainItem {
 }
 
 extension DomainItem {
-//    public func sign(message: String) async throws -> String {
-//        guard let ownerAddress = self.ownerWallet,
-//              let ownerWallet = appContext.udWalletsService.find(by: ownerAddress) else {
-//            throw NetworkLayerError.failedToFindOwnerWallet
-//        }
-//        return try await ownerWallet.getCryptoSignature(messageString: message)
-//    }
-    
     public func ethSign(message: String) async throws -> String {
         guard let ownerAddress = self.ownerWallet,
               let ownerWallet = appContext.udWalletsService.find(by: ownerAddress) else {
