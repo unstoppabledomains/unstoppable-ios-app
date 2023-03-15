04b6836116ef5bfc04ab04b6cb0254b64c4660bf
rommex <roman@unstoppabledomains.com>
2023-03-15 14:06:09 +0200

    removed 'only one dApp session' constraint

--- unstoppable-ios-app/domains-manager-ios/Services/WalletConnect/WalletConnectService/WCConnectedAppsStorage.swift
+++ unstoppable-ios-app/domains-manager-ios/Services/WalletConnect/WalletConnectService/WCConnectedAppsStorage.swift
@@ -98,8 +98,4 @@ class WCConnectedAppsStorage: DefaultsStorage<WCConnectedAppsStorage.ConnectedAp
     func findBy(domainName: DomainName) -> [ConnectedApp]? {
         return retrieveApps().filter({ $0.domain.name == domainName } )
     }
-    
-    func findDuplicate(to newApp: ConnectedApp) -> [ConnectedApp] {
-        return retrieveApps().filter({ $0.appUrl.host == newApp.appUrl.host } )
-    }
 }
