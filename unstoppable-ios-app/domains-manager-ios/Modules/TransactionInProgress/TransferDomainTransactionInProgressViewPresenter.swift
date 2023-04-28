5d99d280ad8aafd67a0583fe5b8493a35aa91415
Oleg Kuplin <>
2023-04-28 20:51:51 +0700

    Fixed missing event case in TransferDomainTransactionInProgressViewPresenter

--- unstoppable-ios-app/domains-manager-ios/Modules/TransactionInProgress/TransferDomainTransactionInProgressViewPresenter.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/TransactionInProgress/TransferDomainTransactionInProgressViewPresenter.swift
@@ -89,7 +89,7 @@ extension TransferDomainTransactionInProgressViewPresenter: ExternalEventsServic
             switch event {
             case .recordsUpdated, .reverseResolutionSet, .reverseResolutionRemoved, .domainTransferred:
                 refreshMintingTransactions()
-            case .wcDeepLink, .walletConnectRequest, .mintingFinished, .domainProfileUpdated:
+            case .wcDeepLink, .walletConnectRequest, .mintingFinished, .domainProfileUpdated, .badgeAdded:
                 return
             }
         }
