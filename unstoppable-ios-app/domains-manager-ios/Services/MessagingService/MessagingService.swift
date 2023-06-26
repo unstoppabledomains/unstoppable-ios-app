042d1813ac7910f149fe771a3174ed14201eede8
Oleg Kuplin <oleg@unstoppabledomains.com>
2023-06-26 13:54:56 +0700

    Return wallet address as available user to message

--- unstoppable-ios-app/domains-manager-ios/Services/MessagingService/MessagingService.swift
+++ unstoppable-ios-app/domains-manager-ios/Services/MessagingService/MessagingService.swift
@@ -318,11 +318,12 @@ extension MessagingService: MessagingServiceProtocol {
     func searchForUsersWith(searchKey: String) async throws -> [MessagingChatUserDisplayInfo] {
         guard searchKey.isValidAddress() else { return [] }
         
-        if let userInfo = await loadUserInfoFor(wallet: searchKey) {
+        let wallet = searchKey
+        if let userInfo = await loadUserInfoFor(wallet: wallet) {
             return [userInfo]
         }
         
-        return []
+        return [.init(wallet: wallet)]
     }
     
     func searchForChannelsWith(page: Int,
