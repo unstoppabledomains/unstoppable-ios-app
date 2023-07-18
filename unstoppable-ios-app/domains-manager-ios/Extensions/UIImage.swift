d3c825df63bb92d8296c4f7d89e5ae79381a8abc
Oleg Kuplin <oleg@unstoppabledomains.com>
2023-07-18 15:03:05 +0700

    Improved UI of image message placeholder

--- unstoppable-ios-app/domains-manager-ios/Extensions/UIImage.swift
+++ unstoppable-ios-app/domains-manager-ios/Extensions/UIImage.swift
@@ -290,6 +290,19 @@ extension UIImage {
         
         return image
     }
+    
+    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+        let scaleFactor = min(widthRatio, heightRatio)
+        let scaledImageSize = CGSize(width: size.width * scaleFactor, height: size.height * scaleFactor)
+        let renderer = UIGraphicsImageRenderer(size: scaledImageSize)
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: scaledImageSize))
+        }
+        
+        return scaledImage
+    }
 }
 
 extension UIImage {
--- unstoppable-ios-app/domains-manager-ios/Modules/Messaging/Chat/Cells/ChatImageCell/ChatImageCell.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/Messaging/Chat/Cells/ChatImageCell/ChatImageCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class ChatImageCell: ChatUserMessageCell {
 
@@ -20,7 +21,7 @@ final class ChatImageCell: ChatUserMessageCell {
         
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 12
-        imageView.contentMode = .scaleAspectFill
+        imageView.tintColor = .foregroundDefault
         setImageSize(.square(size: maxSize))
     }
 
@@ -31,15 +32,26 @@ extension ChatImageCell {
     func setWith(configuration: ChatViewController.ImageBase64MessageUIConfiguration) {
         self.actionCallback = configuration.actionCallback
         setWith(message: configuration.message)
-        setImage(configuration.imageMessageDisplayInfo.image ?? .domainSharePlaceholder)
+        setImage(configuration.imageMessageDisplayInfo.image)
     }
 }
 
 // MARK: - Private methods
 private extension ChatImageCell {
-    func setImage(_ image: UIImage) {
-        imageView.image = image
-        let imageSize = image.size
+    func setImage(_ image: UIImage?) {
+        let imageSize: CGSize
+        if let image {
+            imageView.image = image
+            imageSize = image.size
+            imageView.backgroundColor = .clear
+            imageView.contentMode = .scaleAspectFill
+        } else {
+            let image = UIImage.framesIcon.scalePreservingAspectRatio(targetSize: .square(size: 100))
+            imageView.image = image
+            imageSize = .square(size: 100)
+            imageView.backgroundColor = .backgroundMuted2
+            imageView.contentMode = .center
+        }
         let imageViewSize: CGSize
         
         if imageSize.width > imageSize.height {
@@ -65,4 +77,29 @@ private extension ChatImageCell {
         imageViewConstraints = [widthConstraint, heightConstraint]
         NSLayoutConstraint.activate(imageViewConstraints)
     }
+}
+
+struct ChatImageCell_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        let height: CGFloat = 76
+        
+        return UICollectionViewCellPreview(cellType: ChatImageCell.self, height: height) { cell in
+            let user = MessagingChatUserDisplayInfo(wallet: "24")
+            var imageDetails = MessagingChatMessageImageBase64TypeDisplayInfo(base64: "", encryptedContent: "")
+//            imageDetails.image = .appleIcon
+            let message = MessagingChatMessageDisplayInfo(id: "1",
+                                                          chatId: "2",
+                                                          senderType: .otherUser(user),
+                                                          time: Date(),
+                                                          type: .imageBase64(imageDetails),
+                                                          isRead: false,
+                                                          isFirstInChat: true,
+                                                          deliveryState: .delivered,
+                                                          isEncrypted: false)
+            cell.setWith(configuration: .init(message: message, imageMessageDisplayInfo: imageDetails, actionCallback: { _ in }))
+        }
+        .frame(width: 390, height: 390)
+    }
+    
 }
