ee33605c2ad3f66e98ed5ae26421211581d38067
Oleg Kuplin <oleg@unstoppabledomains.com>
2023-08-11 14:31:01 +0700

    Fixed crash when user search for domains with unsupported url characters

--- unstoppable-ios-app/domains-manager-ios/Services/Networking/NetworkService.swift
+++ unstoppable-ios-app/domains-manager-ios/Services/Networking/NetworkService.swift
@@ -364,7 +364,8 @@ extension NetworkService {
     /// This function will return UD/ENS/Null name and corresponding PFP if available OR throw 404
     func fetchGlobalReverseResolution(for address: HexAddress) async throws -> GlobalRR? {
         do {
-            let url = URL(string: "\(NetworkConfig.baseProfileAPIUrl)/profile/resolve/\(address)")!
+            guard let url = URL(string: "\(NetworkConfig.baseProfileAPIUrl)/profile/resolve/\(address)") else { return nil } // User's input contains not allowed characters
+            
             let data = try await NetworkService().fetchData(for: url,
                                                             method: .get,
                                                             extraHeaders: MetadataNetworkConfig.authHeader)
