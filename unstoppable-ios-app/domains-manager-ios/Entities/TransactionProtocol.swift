9200140cd1a050531a7df99fac96c59ac1ce4a70
rommex <roman@unstoppabledomains.com>
2023-12-01 15:19:06 +0200

    added 2 new ones

--- unstoppable-ios-app/domains-manager-ios/Entities/TransactionProtocol.swift
+++ unstoppable-ios-app/domains-manager-ios/Entities/TransactionProtocol.swift
@@ -57,6 +57,8 @@ enum TxOperation: String, Codable {
     case trackPolygonCheckpoint = "TrackPolygonCheckpoint"
     case withdrawToEthereum = "WithdrawToEthereum"
     case ensRenew = "EnsRenew"
+    case ensSetResolver = "ensSetResolver"
+    case ensResolverRecordsUpdate = "ensResolverRecordsUpdate"
     
     case bulkIssue = "BulkIssue"
     case approvalForAll = "ApprovalForAll"
