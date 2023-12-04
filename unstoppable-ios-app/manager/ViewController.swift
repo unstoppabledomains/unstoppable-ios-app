74e22ef951768d0884b89425fdd4e285e0308025
Oleg Kuplin <oleg@unstoppabledomains.com>
2023-12-04 12:11:14 +0700

    Added fonts to preview

--- unstoppable-ios-app/manager/ViewController.swift
+++ unstoppable-ios-app/manager/ViewController.swift
@@ -5,13 +5,25 @@
 //  Created by Oleg Kuplin on 01.12.2023.
 //
 
-import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let view = PurchaseDomainsCheckoutView(domain: .init(name: "oleg.x", price: 10000, metadata: nil),
+                                               selectedWallet: WalletWithInfo.mock[0],
+                                               wallets: WalletWithInfo.mock,
+                                               purchasedCallback: { [weak self] in
+            
+        },
+                                               scrollOffsetCallback: { [weak self] offset in
+            
+        })
+        
+        let vc = UIHostingController(rootView: view)
+        addChildViewController(vc, andEmbedToView: self.view)
     }
 
 
