28b7605c6a42e04247fe5b73522246dfafacbdab
Oleg Kuplin <oleg@unstoppabledomains.com>
2023-12-04 09:36:51 +0700

    Added checkout module to preview target

--- unstoppable-ios-app/manager/Entities/PreviewUDWallet.swift
+++ unstoppable-ios-app/manager/Entities/PreviewUDWallet.swift
@@ -11,6 +11,8 @@ struct WalletWithInfo {
     var wallet: UDWallet
     var displayInfo: WalletDisplayInfo?
     
+    var address: String { wallet.address }
+    
     static let mock: [WalletWithInfo] = UDWallet.mock.map { WalletWithInfo(wallet: $0, displayInfo: .init(wallet: $0, domainsCount: Int(arc4random_uniform(3)), udDomainsCount: Int(arc4random_uniform(3))))}
     
 }
