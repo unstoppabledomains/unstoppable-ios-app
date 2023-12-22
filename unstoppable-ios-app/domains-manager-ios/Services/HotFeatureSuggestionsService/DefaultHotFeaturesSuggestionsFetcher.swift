8b17ce0d1f40436080477eb5af6c91e94ea32f80
Oleg Kuplin <oleg@unstoppabledomains.com>
2023-12-22 15:19:38 +0700

    Added API call description to hot features fetcher

--- unstoppable-ios-app/domains-manager-ios/Services/HotFeatureSuggestionsService/DefaultHotFeaturesSuggestionsFetcher.swift
+++ unstoppable-ios-app/domains-manager-ios/Services/HotFeatureSuggestionsService/DefaultHotFeaturesSuggestionsFetcher.swift
@@ -9,7 +9,12 @@ import Foundation
 
 struct DefaultHotFeaturesSuggestionsFetcher: HotFeaturesSuggestionsFetcher {
     func loadHotFeatureSuggestions() async throws -> [HotFeatureSuggestion] {
-        []
+        let request = try APIRequest(urlString: "https://pr-8599.api.ud-staging.com/api/v1/resellers/mobile-app-v1/mobile-hot-suggestions",
+                                 method: .get)
+        
+        let response: SuggestionsResponse = try await NetworkService().makeDecodableAPIRequest(request)
+        
+        return response.suggestions
     }
     
     private struct SuggestionsResponse: Codable {
