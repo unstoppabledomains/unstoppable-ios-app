2b266674e34dbb5c61b197227e6190d351c1232f
Oleg <oleg@unstoppabledomains.com>
2024-01-10 13:50:40 +0700

    Restore log topics

--- unstoppable-ios-app/domains-manager-ios/SupportingFiles/AppDelegate.swift
+++ unstoppable-ios-app/domains-manager-ios/SupportingFiles/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         #if DEBUG
-        Debugger.setAllowedTopicsSet(.custom([.Analytics]))
+        Debugger.setAllowedTopicsSet(.debugDefault)
 //        CoreDataMessagingStorageService(decrypterService: AESMessagingContentDecrypterService()).clear()
 //        MessagingFilesService(decrypterService: AESMessagingContentDecrypterService()).clear()
 //        Task {
