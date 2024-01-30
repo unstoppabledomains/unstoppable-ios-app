c2ddca105d7f332748fba2d1a34d15995a5d07fa
Oleg <oleg@unstoppabledomains.com>
2024-01-30 22:10:11 +0700

    Improved NFT dates formatting

--- unstoppable-ios-app/domains-manager-ios/Extensions/Codable.swift
+++ unstoppable-ios-app/domains-manager-ios/Extensions/Codable.swift
@@ -143,22 +143,36 @@ extension JSONDecoder.DateDecodingStrategy {
     static func iso8601WithOptions(_ options: ISO8601DateFormatter.Options) -> JSONDecoder.DateDecodingStrategy {
         let formatter = ISO8601DateFormatter()
         formatter.formatOptions.insert(options)
-        return dateStrategyUsing(formatter: formatter)
+        return dateStrategyUsing(formatters: [formatter])
+    }
+    
+    private static func noTimeZoneIndicatorDateFormatter() -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        return formatter
     }
     
     static func noTimeZoneIndicatorDecodingStrategy() -> JSONDecoder.DateDecodingStrategy {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        let formatter = noTimeZoneIndicatorDateFormatter()
         
-        return dateStrategyUsing(formatter: formatter)
+        return dateStrategyUsing(formatters: [formatter])
     }
     
-    static func dateStrategyUsing(formatter: DateFormatterProtocol) -> JSONDecoder.DateDecodingStrategy {
+    static func nftDateDecodingStrategy() -> JSONDecoder.DateDecodingStrategy {
+        let formatter = noTimeZoneIndicatorDateFormatter()
+        let isoFormatter = ISO8601DateFormatter()
+        isoFormatter.formatOptions.insert([.withFractionalSeconds])
+        return dateStrategyUsing(formatters: [formatter, isoFormatter])
+    }
+    
+    static func dateStrategyUsing(formatters: [DateFormatterProtocol]) -> JSONDecoder.DateDecodingStrategy {
         .custom { decoder in
             let container = try decoder.singleValueContainer()
             let dateString = try container.decode(String.self)
-            if let date = formatter.date(from: dateString) {
-                return date
+            for formatter in formatters {
+                if let date = formatter.date(from: dateString) {
+                    return date
+                }
             }
             
             throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode date string \(dateString)")
