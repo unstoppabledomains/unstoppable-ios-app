5f361cb87c696222ce665c08ef130e66b964e3fb
Oleg <oleg@unstoppabledomains.com>
2024-02-05 20:29:06 +0700

    Code review updates

--- unstoppable-ios-app/domains-manager-ios/Extensions/Task.swift
+++ unstoppable-ios-app/domains-manager-ios/Extensions/Task.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Task where Success == Never, Failure == Never {
     static func sleep(seconds duration: TimeInterval) async {
-        let duration = UInt64(duration * 1_000_000_000)
-        try? await Task.sleep(nanoseconds: duration)
+        let durationInNanoSec = UInt64(duration * 1_000_000_000)
+        try? await Task.sleep(nanoseconds: durationInNanoSec)
     }
 }
