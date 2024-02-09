d6547d09a3d488516f342fc95b06a0cef1e9c016
Oleg <oleg@unstoppabledomains.com>
2024-02-09 11:22:49 +0700

    Added all configurations for preview

--- unstoppable-ios-app/domains-manager-ios-preview/Modules/PreviewWCRequests/PreviewWCRequests.swift
+++ unstoppable-ios-app/domains-manager-ios-preview/Modules/PreviewWCRequests/PreviewWCRequests.swift
@@ -13,14 +13,12 @@ import SwiftUI
     vc.view.backgroundColor = .white
     
     DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-        
-        
-        
-        
-        
         Task {
-            try? await appContext.pullUpViewService.showServerConnectConfirmationPullUp(for: createConnectConfiguration(),
-                                                                             in: vc)
+            let connectConfiguration = createConnectConfiguration()
+            let signConfiguration = createSignConfiguration()
+            let paymentConfiguration = createPaymentConfiguration()
+            _ = try? await appContext.pullUpViewService.showServerConnectConfirmationPullUp(for: connectConfiguration,
+                                                                                            in: vc)
         }
     }
     
@@ -35,6 +33,31 @@ private func createConnectConfiguration() -> WCRequestUIConfiguration {
 
 private func createWalletConnectConfig() -> WalletConnectServiceV2.ConnectionConfig {
     .init(domain: connectionDomain(), appInfo: connectedAppInfo())
+}
+
+private func createSignConfiguration() -> WCRequestUIConfiguration {
+    .signMessage(createSignMessageTransactionUIConfiguration())
+}
+
+private func createSignMessageTransactionUIConfiguration() -> SignMessageTransactionUIConfiguration {
+    .init(connectionConfig: createWalletConnectConfig(), signingMessage: "lakjsdasdjalsdjaslkdjalsdkj ald")
+}
+
+private func createPaymentConfiguration() -> WCRequestUIConfiguration {
+    .payment(createSignPaymentTransactionUIConfiguration())
+}
+
+private func createSignPaymentTransactionUIConfiguration() -> SignPaymentTransactionUIConfiguration {
+    .init(connectionConfig: createWalletConnectConfig(),
+          walletAddress: "0xc4a748796805dfa42cafe0901ec182936584cc6e",
+          chainId: 1,
+          cost: createTxDisplayDetails())
+}
+
+private func createTxDisplayDetails() -> SignPaymentTransactionUIConfiguration.TxDisplayDetails {
+    .init(tx: .init(value: .init(quantity: 100000000000000),
+                    gasPrice: .init(quantity: 2300000000),
+                    gas: .init(quantity: 350000)))!
 }
 
 private func connectionDomain() -> DomainItem {
