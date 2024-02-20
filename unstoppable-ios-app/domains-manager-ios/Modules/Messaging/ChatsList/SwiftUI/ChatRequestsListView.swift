6fec865781121a920bd0a1f1b2ff4a6f995c6610
Oleg <oleg@unstoppabledomains.com>
2024-02-20 14:38:15 +0700

    Created chat requests list view

---
+++ unstoppable-ios-app/domains-manager-ios/Modules/Messaging/ChatsList/SwiftUI/ChatRequestsListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRequestsListView.swift
+//  domains-manager-ios
+//
+//  Created by Oleg Kuplin on 20.02.2024.
+//
+
+import SwiftUI
+
+struct ChatRequestsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatRequestsListView()
+}
