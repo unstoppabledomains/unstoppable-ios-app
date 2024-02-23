be213e9418edb522a03dc2ee8e7919540cc0e250
Oleg <oleg@unstoppabledomains.com>
2024-02-23 08:16:24 +0700

    Created HomeExploreView

---
+++ unstoppable-ios-app/domains-manager-ios/Modules/Explore/HomeExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeExploreView.swift
+//  domains-manager-ios
+//
+//  Created by Oleg Kuplin on 23.02.2024.
+//
+
+import SwiftUI
+
+struct HomeExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeExploreView()
+}
