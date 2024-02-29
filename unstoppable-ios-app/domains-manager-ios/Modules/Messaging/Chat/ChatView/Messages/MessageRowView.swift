e5064dd807bc2448c31917410b880a51f345ca9f
Oleg <oleg@unstoppabledomains.com>
2024-02-29 21:05:04 +0700

    Adjusted UI of reactions in chat (#404)
    * Adjusted UI of reactions in chat
    
    * Adjusted spacings in chat

--- unstoppable-ios-app/domains-manager-ios/Modules/Messaging/Chat/ChatView/Messages/MessageRowView.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/Messaging/Chat/ChatView/Messages/MessageRowView.swift
@@ -17,9 +17,10 @@ struct MessageRowView: View, ViewAnalyticsLogger {
     let isGroupChatMessage: Bool
     @State private var otherUserAvatar: UIImage?
     @State private var showingReactionsPopover = false
+    private let groupChatImageXOffset: CGFloat = 46
     
     var body: some View {
-        VStack(alignment: message.senderType.isThisUser ? .trailing : .leading) {
+        VStack(alignment: message.senderType.isThisUser ? .trailing : .leading, spacing: 2) {
             HStack(alignment: .bottom, spacing: 8) {
                 if message.senderType.isThisUser {
                     Spacer()
@@ -36,17 +37,17 @@ struct MessageRowView: View, ViewAnalyticsLogger {
             }
             
             HStack {
-                if isGroupChatMessage, !isThisUser {
-                    Spacer()
-                        .frame(width: 46)
-                }
-                timeView()
+                groupAvatarXOffsetViewOfNeeded()
+                underMessageView()
                 if isFailedMessage, isThisUser {
                     Spacer()
                         .frame(width: 40)
                 }
             }
-            reactionsView()
+            HStack {
+                groupAvatarXOffsetViewOfNeeded()
+                reactionsView()
+            }
         }
         .frame(maxWidth: .infinity)
         .background(Color.clear)
@@ -61,6 +62,7 @@ private extension MessageRowView {
     }
     var sender: MessagingChatSender { message.senderType }
     var isThisUser: Bool { sender.isThisUser }
+    var isNeedToAddGroupAvatarXOffset: Bool { isGroupChatMessage && !isThisUser }
     
     @ViewBuilder
     func messageContentView() -> some View {
@@ -103,12 +105,25 @@ private extension MessageRowView {
     }
     
     @ViewBuilder
-    func timeView() -> some View {
+    func underMessageView() -> some View {
         if isFailedMessage {
             failedToSendMessageView()
         } else {
             timeLabelView()
         }
+    }
+    
+    @ViewBuilder
+    func groupAvatarXOffsetViewOfNeeded() -> some View {
+        if isNeedToAddGroupAvatarXOffset {
+            groupAvatarXOffsetView()
+        }
+    }
+    
+    @ViewBuilder
+    func groupAvatarXOffsetView() -> some View {
+        Spacer()
+            .frame(width: groupChatImageXOffset)
     }
     
     var timeLabelText: String {
@@ -250,7 +265,7 @@ private extension MessageRowView {
             .foregroundStyle(Color.foregroundDefault)
             .padding(.init(horizontal: 8, vertical: 8))
             .background(Color.backgroundMuted)
-            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .clipShape(Capsule())
         }
         .buttonStyle(.plain)
         .alwaysPopover(isPresented: $showingReactionsPopover) {
