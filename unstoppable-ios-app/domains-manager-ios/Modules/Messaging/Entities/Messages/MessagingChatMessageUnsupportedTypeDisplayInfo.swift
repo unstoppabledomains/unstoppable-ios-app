e7b13d4e62f557ee291a1d31ae90f780bab93377
Oleg <oleg@unstoppabledomains.com>
2024-03-05 16:36:48 +0700

    Dev/fix/messaging fixes (#414)
    * Fixed UI for long chat title
    
    * Added unsupported message type
    
    * Filter unsupported messages on the chat view
    
    * Break load more messages loop after 30 sec

---
+++ unstoppable-ios-app/domains-manager-ios/Modules/Messaging/Entities/Messages/MessagingChatMessageUnsupportedTypeDisplayInfo.swift
@@ -0,0 +1,12 @@
+//
+//  MessagingChatMessageUnsupportedTypeDisplayInfo.swift
+//  domains-manager-ios
+//
+//  Created by Oleg Kuplin on 05.03.2024.
+//
+
+import Foundation
+
+struct MessagingChatMessageUnsupportedTypeDisplayInfo: Hashable {
+    let data: Data
+}
