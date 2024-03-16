7a3c1abfe0329db3a459695f02c0dc171faaa581
Oleg <oleg@unstoppabledomains.com>
2024-03-16 15:30:17 +0700

    Break mpc flow into steps. Visualise steps on the UI

--- unstoppable-ios-app/domains-manager-ios/Modules/MPC/EnterPassphrase/MPCEnterPassphraseView.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/MPC/EnterPassphrase/MPCEnterPassphraseView.swift
@@ -14,16 +14,24 @@ struct MPCEnterPassphraseView: View {
     @State private var input: String = ""
     @State private var isLoading = false
     @State private var error: Error?
+    @State private var mpcState: String = ""
 
     var body: some View {
-        VStack(spacing: 32) {
-            headerView()
-            inputView()
-            actionButtonView()
-            Spacer()
+        ZStack {
+            VStack(spacing: 32) {
+                headerView()
+                inputView()
+                actionButtonView()
+                Spacer()
+            }
+            .padding()
+            .padding(EdgeInsets(top: 70, leading: 0, bottom: 0, trailing: 0))
+            if isLoading {
+                Color.black.opacity(0.3)
+                mpcStateView()
+            }
         }
-        .padding()
-        .padding(EdgeInsets(top: 70, leading: 0, bottom: 0, trailing: 0))
+        .ignoresSafeArea()
         .animation(.default, value: UUID())
         .displayError($error)
     }
@@ -66,15 +74,46 @@ private extension MPCEnterPassphraseView {
     
     func actionButtonPressed() {
         Task {
+            await KeyboardService.shared.hideKeyboard()
+            
             isLoading = true
             do {
-                let mpcWallet = try await MPCNetworkService.shared.signForNewDeviceWith(code: code, recoveryPhrase: input)
-                mpcWalletCreatedCallback(mpcWallet)
+                let mpcWalletStepsStream = MPCNetworkService.shared.signForNewDeviceWith(code: code, recoveryPhrase: input)
+                
+                for try await step in mpcWalletStepsStream {
+                    updateForSetupMPCWalletStep(step)
+                }
             } catch {
                 self.error = error
             }
             isLoading = false
         }
+    }
+    
+    func updateForSetupMPCWalletStep(_ step: SetupMPCWalletStep) {
+        mpcState = step.title
+        switch step {
+        case .finished(let mpcWallet):
+            mpcWalletCreatedCallback(mpcWallet)
+        default:
+            return
+        }
+    }
+    
+    @ViewBuilder
+    func mpcStateView() -> some View {
+        VStack(spacing: 20) {
+            ProgressView()
+                .tint(Color.foregroundDefault)
+            Text(mpcState)
+                .bold()
+                .multilineTextAlignment(.center)
+        }
+        .foregroundStyle(Color.foregroundDefault)
+        .frame(width: 300, height: 150)
+        .background(Color.backgroundDefault)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .padding()
     }
 }
 #Preview {
--- unstoppable-ios-app/domains-manager-ios/Modules/MPC/MPCNetworkService.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/MPC/MPCNetworkService.swift
@@ -66,86 +66,101 @@ extension MPCNetworkService {
         
         logMPC("Did receive MPC bootstrap code: \(response.code)")
     }
-    
+
     func signForNewDeviceWith(code: String,
-                              recoveryPhrase: String) async throws -> UDMPCWallet {
-        struct Body: Encodable {
-            let code: String
-            var device: String? = nil
-        }
-        
-        struct Response: Decodable {
-            let accessToken: String // temp access token
-            let deviceId: String
-        }
-        
-        logMPC("Will submit code \(code). recoveryPhrase: \(recoveryPhrase)")
-        let body = Body(code: code)
-        let request = try APIRequest(urlString: MPCNetwork.URLSList.submitCodeURL,
-                                     body: body,
-                                     method: .post)
-        
-        let response: Response = try await makeDecodableAPIRequest(request)
-        logMPC("Did submit code \(code)")
-        let accessToken = response.accessToken
-        let deviceId = response.deviceId
-        
-        let rpcHandler = FireblocksBootstrapRPCMessageHandler(authToken: accessToken)
-        logMPC("Will create fireblocks connector")
-        let fireblocksConnector = try FireblocksConnector(deviceId: deviceId,
-                                                          messageHandler: rpcHandler)
-        fireblocksConnector.stopJoinWallet()
-        logMPC("Did create fireblocks connector")
-        logMPC("Will request to join existing wallet")
-        do {
-            let requestId = try await fireblocksConnector.requestJoinExistingWallet()
-            logMPC("Will auth new device with request id: \(requestId)")
-            // Once we have the key material, now it’s time to get a full access token to the Wallets API. To prove that the key material is valid, you need to create a transaction to sign
-            // Initialize a transaction with the Wallets API
-            try await authNewDeviceWith(requestId: requestId,
-                                        recoveryPhrase: recoveryPhrase,
-                                        accessToken: accessToken)
-            logMPC("Did auth new device with request id: \(requestId)")
-            logMPC("Will wait for key is ready")
-            try await fireblocksConnector.waitForKeyIsReady()
-            
-            logMPC("Will init transaction with new key materials")
-            let transactionDetails = try await initTransactionWithNewKeyMaterials(accessToken: accessToken)
-            let txId = transactionDetails.transactionId
-            logMPC("Did init transaction with new key materials with tx id: \(txId)")
-            
-            /// Skipping this part because iOS doesn't have equal functions. To discuss with Wallet team
-            /*
-             const inProg = await sdk.getInProgressSigningTxId();
-             if (inProg && inProg !== tx.transactionId) {
-             this.logger.warn('Encountered in progress tx', { inProg });
-             await sdk.stopInProgressSignTransaction();
-             }
-             */
-            
-            //    We have to wait for Fireblocks to also sign, so poll the Wallets API until the transaction is returned with the PENDING_SIGNATURE status
-            logMPC("Will wait for transaction with new key materials is ready with tx id: \(txId)")
-            try await waitForTransactionWithNewKeyMaterialsReady(accessToken: accessToken)
-            
-            logMPC("Will sign transaction with fireblocks. txId: \(txId)")
-            try await fireblocksConnector.signTransactionWith(txId: txId)
-            
-            //    Once it is pending a signature, sign with the Fireblocks NCW SDK and confirm with the Wallets API that you have signed. After confirmation is validated, you’ll be returned an access token, a refresh token and a bootstrap token.
-            logMPC("Will confirm transaction is signed")
-            let finalAuthResponse = try await confirmTransactionWithNewKeyMaterialsSigned(accessToken: accessToken)
-            logMPC("Did confirm transaction is signed")
-            
-            logMPC("Will verify final response \(finalAuthResponse)")
-            try await verifyAccessToken(finalAuthResponse.accessToken)
-            logMPC("Did verify verify final response \(finalAuthResponse) success")
-            
-            let mpcWallet = UDMPCWallet(deviceId: deviceId,
-                                        tokens: .init(refreshToken: finalAuthResponse.refreshToken,
-                                                      bootstrapToken: finalAuthResponse.bootstrapToken))
-            return mpcWallet
-        } catch {
-            fireblocksConnector.stopJoinWallet()
-            throw error 
+                              recoveryPhrase: String) -> AsyncThrowingStream<SetupMPCWalletStep, Error> {
+        AsyncThrowingStream { continuation in
+            Task {
+                struct Body: Encodable {
+                    let code: String
+                    var device: String? = nil
+                }
+                
+                struct Response: Decodable {
+                    let accessToken: String // temp access token
+                    let deviceId: String
+                }
+                
+                continuation.yield(.submittingCode)
+                logMPC("Will submit code \(code). recoveryPhrase: \(recoveryPhrase)")
+                let body = Body(code: code)
+                let request = try APIRequest(urlString: MPCNetwork.URLSList.submitCodeURL,
+                                             body: body,
+                                             method: .post)
+                
+                let response: Response = try await makeDecodableAPIRequest(request)
+                logMPC("Did submit code \(code)")
+                let accessToken = response.accessToken
+                let deviceId = response.deviceId
+                
+                continuation.yield(.initialiseFireblocks)
+                let rpcHandler = FireblocksBootstrapRPCMessageHandler(authToken: accessToken)
+                logMPC("Will create fireblocks connector")
+                let fireblocksConnector = try FireblocksConnector(deviceId: deviceId,
+                                                                  messageHandler: rpcHandler)
+                fireblocksConnector.stopJoinWallet()
+                logMPC("Did create fireblocks connector")
+                logMPC("Will request to join existing wallet")
+                do {
+                    continuation.yield(.requestingToJoinExistingWallet)
+                    let requestId = try await fireblocksConnector.requestJoinExistingWallet()
+                    logMPC("Will auth new device with request id: \(requestId)")
+                    // Once we have the key material, now it’s time to get a full access token to the Wallets API. To prove that the key material is valid, you need to create a transaction to sign
+                    // Initialize a transaction with the Wallets API
+                    continuation.yield(.authorisingNewDevice)
+                    try await authNewDeviceWith(requestId: requestId,
+                                                recoveryPhrase: recoveryPhrase,
+                                                accessToken: accessToken)
+                    logMPC("Did auth new device with request id: \(requestId)")
+                    logMPC("Will wait for key is ready")
+                    continuation.yield(.waitingForKeysIsReady)
+                    try await fireblocksConnector.waitForKeyIsReady()
+                    
+                    logMPC("Will init transaction with new key materials")
+                    continuation.yield(.initialiseTransaction)
+                    let transactionDetails = try await initTransactionWithNewKeyMaterials(accessToken: accessToken)
+                    let txId = transactionDetails.transactionId
+                    logMPC("Did init transaction with new key materials with tx id: \(txId)")
+                    
+                    /// Skipping this part because iOS doesn't have equal functions. To discuss with Wallet team
+                    /*
+                     const inProg = await sdk.getInProgressSigningTxId();
+                     if (inProg && inProg !== tx.transactionId) {
+                     this.logger.warn('Encountered in progress tx', { inProg });
+                     await sdk.stopInProgressSignTransaction();
+                     }
+                     */
+                    
+                    //    We have to wait for Fireblocks to also sign, so poll the Wallets API until the transaction is returned with the PENDING_SIGNATURE status
+                    logMPC("Will wait for transaction with new key materials is ready with tx id: \(txId)")
+                    continuation.yield(.waitingForTransactionIsReady)
+                    try await waitForTransactionWithNewKeyMaterialsReady(accessToken: accessToken)
+                    
+                    logMPC("Will sign transaction with fireblocks. txId: \(txId)")
+                    continuation.yield(.signingTransaction)
+                    try await fireblocksConnector.signTransactionWith(txId: txId)
+                    
+                    //    Once it is pending a signature, sign with the Fireblocks NCW SDK and confirm with the Wallets API that you have signed. After confirmation is validated, you’ll be returned an access token, a refresh token and a bootstrap token.
+                    logMPC("Will confirm transaction is signed")
+                    continuation.yield(.confirmingTransaction)
+                    let finalAuthResponse = try await confirmTransactionWithNewKeyMaterialsSigned(accessToken: accessToken)
+                    logMPC("Did confirm transaction is signed")
+                    
+                    logMPC("Will verify final response \(finalAuthResponse)")
+                    continuation.yield(.verifyingAccessToken)
+                    try await verifyAccessToken(finalAuthResponse.accessToken)
+                    logMPC("Did verify verify final response \(finalAuthResponse) success")
+                    
+                    let mpcWallet = UDMPCWallet(deviceId: deviceId,
+                                                tokens: .init(refreshToken: finalAuthResponse.refreshToken,
+                                                              bootstrapToken: finalAuthResponse.bootstrapToken))
+                    continuation.yield(.finished(mpcWallet))
+                    continuation.finish()
+                } catch {
+                    fireblocksConnector.stopJoinWallet()
+                    continuation.finish(throwing: error)
+                }
+            }
         }
     }
 }
@@ -300,3 +315,44 @@ extension NetworkAuthorisedWithBearerService {
     }
 }
 
+
+enum SetupMPCWalletStep {
+    case submittingCode
+    case initialiseFireblocks
+    case requestingToJoinExistingWallet
+    case authorisingNewDevice
+    case waitingForKeysIsReady
+    case initialiseTransaction
+    case waitingForTransactionIsReady
+    case signingTransaction
+    case confirmingTransaction
+    case verifyingAccessToken
+    case finished(UDMPCWallet)
+    
+    var title: String {
+        switch self {
+        case .submittingCode:
+            "Submitting code"
+        case .initialiseFireblocks:
+            "Initialise Fireblocks"
+        case .requestingToJoinExistingWallet:
+            "Requesting to join existing wallet"
+        case .authorisingNewDevice:
+            "Authorising new device"
+        case .waitingForKeysIsReady:
+            "Waiting for keys is ready"
+        case .initialiseTransaction:
+            "Initialise transaction"
+        case .waitingForTransactionIsReady:
+            "Waiting for transaction is ready"
+        case .signingTransaction:
+            "Signing transaction"
+        case .confirmingTransaction:
+            "Confirming transaction"
+        case .verifyingAccessToken:
+            "Verifying access token"
+        case .finished(let uDMPCWallet):
+            "Finished"
+        }
+    }
+}
