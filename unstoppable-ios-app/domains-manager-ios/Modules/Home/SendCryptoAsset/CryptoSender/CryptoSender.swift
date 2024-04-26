ca56f66055ef6d0f273d774f71dda35a82dce422
rommex <roman@unstoppabledomains.com>
2024-04-26 22:42:38 +0300

    moved checks into MPCCryptoSender implementations

--- unstoppable-ios-app/domains-manager-ios/Modules/Home/SendCryptoAsset/CryptoSender/CryptoSender.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/Home/SendCryptoAsset/CryptoSender/CryptoSender.swift
@@ -13,10 +13,6 @@ struct CryptoSender: CryptoSenderProtocol {
     func canSendCrypto(token: CryptoSender.SupportedToken, chain: ChainSpec) -> Bool {
         switch wallet.type {
         case .mpc :
-            guard case .mpc(let symbol) = token else {
-                Debugger.printFailure("MPC wallet attempted to send a non-mpc token: \(token)")
-                return false
-            }
             return MPCCryptoSender(wallet: wallet).canSendCrypto(token: token, chain: chain)
         default:
             return NativeCoinCryptoSender(wallet: wallet)
@@ -29,10 +25,6 @@ struct CryptoSender: CryptoSenderProtocol {
     func sendCrypto(crypto: CryptoSendingSpec, chain: ChainSpec, toAddress: HexAddress) async throws -> String {
         switch wallet.type {
         case .mpc:
-            guard case .mpc(let symbol) = crypto.token else {
-                Debugger.printFailure("MPC wallet attempted to send a non-mpc token: \(crypto.token)")
-                throw CryptoSender.Error.invalidNonMPCTokenSpecification
-            }
             let mpcCryptoSender: CryptoSenderProtocol = MPCCryptoSender(wallet: wallet)
             guard mpcCryptoSender.canSendCrypto(token: crypto.token, chain: chain) else {
                 throw CryptoSender.Error.tokenNotSupportedOnChain
@@ -57,10 +49,6 @@ struct CryptoSender: CryptoSenderProtocol {
     func computeGasFeeFrom(maxCrypto: CryptoSendingSpec, on chain: ChainSpec, toAddress: HexAddress) async throws -> EVMCoinAmount {
         switch wallet.type {
         case .mpc:
-            guard case .mpc(let symbol) = maxCrypto.token else {
-                Debugger.printFailure("MPC wallet attempted to compute a non-mpc token: \(maxCrypto.token)")
-                throw CryptoSender.Error.invalidNonMPCTokenSpecification
-            }
             let mpcCryptoSender: CryptoSenderProtocol = MPCCryptoSender(wallet: wallet)
             guard mpcCryptoSender.canSendCrypto(token: maxCrypto.token, chain: chain) else {
                 throw CryptoSender.Error.tokenNotSupportedOnChain
--- unstoppable-ios-app/domains-manager-ios/Modules/Home/SendCryptoAsset/CryptoSender/MPCCryptoSender.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/Home/SendCryptoAsset/CryptoSender/MPCCryptoSender.swift
@@ -15,18 +15,33 @@ struct MPCCryptoSender: CryptoSenderProtocol {
     }
     
     func canSendCrypto(token: CryptoSender.SupportedToken, chain: ChainSpec) -> Bool {
+        guard case .mpc(let symbol) = token else {
+            Debugger.printFailure("MPC wallet attempted to send a non-mpc token: \(token)")
+            return false
+        }
+
         //TODO:
         #warning("false bool result")
         return false
     }
     
     func sendCrypto(crypto: CryptoSendingSpec, chain: ChainSpec, toAddress: HexAddress) async throws -> String {
+        guard case .mpc(let symbol) = crypto.token else {
+            Debugger.printFailure("MPC wallet attempted to send a non-mpc token: \(crypto.token)")
+            throw CryptoSender.Error.invalidNonMPCTokenSpecification
+        }
+
         //TODO:
         #warning("false result")
         return "false hash 0x0"
     }
     
     func computeGasFeeFrom(maxCrypto: CryptoSendingSpec, on chain: ChainSpec, toAddress: HexAddress) async throws -> EVMCoinAmount {
+        guard case .mpc(let symbol) = maxCrypto.token else {
+            Debugger.printFailure("MPC wallet attempted to compute a non-mpc token: \(maxCrypto.token)")
+            throw CryptoSender.Error.invalidNonMPCTokenSpecification
+        }
+
         //TODO:
         #warning("false error thrown")
         throw CryptoSender.Error.tokenNotSupportedOnChain
