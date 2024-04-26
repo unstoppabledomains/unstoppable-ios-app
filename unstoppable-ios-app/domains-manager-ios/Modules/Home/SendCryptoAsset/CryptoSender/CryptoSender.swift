9a824be5f296763dc7f0ca96d40ea3a2098f298c
rommex <roman@unstoppabledomains.com>
2024-04-26 21:57:33 +0300

    CryptoSender refactored for MPC case

--- unstoppable-ios-app/domains-manager-ios/Modules/Home/SendCryptoAsset/CryptoSender/CryptoSender.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/Home/SendCryptoAsset/CryptoSender/CryptoSender.swift
@@ -11,38 +11,65 @@ struct CryptoSender: CryptoSenderProtocol {
     let wallet: UDWallet
     
     func canSendCrypto(token: CryptoSender.SupportedToken, chain: ChainSpec) -> Bool {
-        // only native tokens supported for Ethereum and Polygon
-        return NativeCoinCryptoSender(wallet: wallet).canSendCrypto(token: token, chain: chain) || TokenCryptoSender(wallet: wallet).canSendCrypto(token: token, chain: chain)
+        switch wallet.type {
+        case .mpc :
+            return MPCCryptoSender(wallet: wallet).canSendCrypto(token: token, chain: chain)
+        default:
+            return NativeCoinCryptoSender(wallet: wallet)
+                            .canSendCrypto(token: token, chain: chain) ||
+                        TokenCryptoSender(wallet: wallet)
+                            .canSendCrypto(token: token, chain: chain)
+        }
     }
-
+    
     func sendCrypto(crypto: CryptoSendingSpec, chain: ChainSpec, toAddress: HexAddress) async throws -> String {
-        let cryptoSender: CryptoSenderProtocol = NativeCoinCryptoSender(wallet: wallet)
-        if cryptoSender.canSendCrypto(token: crypto.token, chain: chain) {
-            return try await cryptoSender.sendCrypto(crypto: crypto, chain: chain, toAddress: toAddress)
+        switch wallet.type {
+        case .mpc:
+            let mpcCryptoSender: CryptoSenderProtocol = MPCCryptoSender(wallet: wallet)
+            guard mpcCryptoSender.canSendCrypto(token: crypto.token, chain: chain) else {
+                throw CryptoSender.Error.tokenNotSupportedOnChain
+            }
+            return try await mpcCryptoSender.sendCrypto(crypto: crypto, chain: chain, toAddress: toAddress)
+            
+        default:
+            let cryptoSender: CryptoSenderProtocol = NativeCoinCryptoSender(wallet: wallet)
+            if cryptoSender.canSendCrypto(token: crypto.token, chain: chain) {
+                return try await cryptoSender.sendCrypto(crypto: crypto, chain: chain, toAddress: toAddress)
+            }
+            
+            let cryptoSender2: CryptoSenderProtocol = TokenCryptoSender(wallet: wallet)
+            if cryptoSender2.canSendCrypto(token: crypto.token, chain: chain) {
+                return try await cryptoSender2.sendCrypto(crypto: crypto, chain: chain, toAddress: toAddress)
+            }
+            throw CryptoSender.Error.sendingNotSupported
+            
         }
-        
-        let cryptoSender2: CryptoSenderProtocol = TokenCryptoSender(wallet: wallet)
-        if cryptoSender2.canSendCrypto(token: crypto.token, chain: chain) {
-            return try await cryptoSender2.sendCrypto(crypto: crypto, chain: chain, toAddress: toAddress)
-        }
-        throw CryptoSender.Error.sendingNotSupported
     }
     
     func computeGasFeeFrom(maxCrypto: CryptoSendingSpec, on chain: ChainSpec, toAddress: HexAddress) async throws -> EVMCoinAmount {
-        let cryptoSender: CryptoSenderProtocol = NativeCoinCryptoSender(wallet: wallet)
-        if cryptoSender.canSendCrypto(token: maxCrypto.token, chain: chain) {
-            return try await cryptoSender.computeGasFeeFrom(maxCrypto: maxCrypto, on: chain, toAddress: toAddress)
+        switch wallet.type {
+        case .mpc:
+            let mpcCryptoSender: CryptoSenderProtocol = MPCCryptoSender(wallet: wallet)
+            guard mpcCryptoSender.canSendCrypto(token: maxCrypto.token, chain: chain) else {
+                throw CryptoSender.Error.tokenNotSupportedOnChain
+            }
+            return try await mpcCryptoSender.computeGasFeeFrom(maxCrypto: maxCrypto, on: chain, toAddress: toAddress)
+            
+        default:
+            let cryptoSender: CryptoSenderProtocol = NativeCoinCryptoSender(wallet: wallet)
+            if cryptoSender.canSendCrypto(token: maxCrypto.token, chain: chain) {
+                return try await cryptoSender.computeGasFeeFrom(maxCrypto: maxCrypto, on: chain, toAddress: toAddress)
+            }
+            
+            let cryptoSender2: CryptoSenderProtocol = TokenCryptoSender(wallet: wallet)
+            if cryptoSender2.canSendCrypto(token: maxCrypto.token, chain: chain) {
+                return try await cryptoSender2.computeGasFeeFrom(maxCrypto: maxCrypto, on: chain, toAddress: toAddress)
+            }
+            throw CryptoSender.Error.sendingNotSupported
         }
-        
-        let cryptoSender2: CryptoSenderProtocol = TokenCryptoSender(wallet: wallet)
-        if cryptoSender2.canSendCrypto(token: maxCrypto.token, chain: chain) {
-            return try await cryptoSender2.computeGasFeeFrom(maxCrypto: maxCrypto, on: chain, toAddress: toAddress)
-        }
-        throw CryptoSender.Error.sendingNotSupported
     }
     
     func fetchGasPrices(on chain: ChainSpec) async throws -> EstimatedGasPrices {
-        let cryptoSender: CryptoSenderProtocol = NativeCoinCryptoSender(wallet: wallet)
-        return try await cryptoSender.fetchGasPrices(on: chain)
+        return try await NetworkService().fetchGasPricesDoubleAttempt(chainId: chain.id)
     }
 }
