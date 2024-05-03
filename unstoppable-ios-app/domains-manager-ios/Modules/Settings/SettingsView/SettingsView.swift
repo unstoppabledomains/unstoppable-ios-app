d211a814394e2056e8ceb16fb0a9f535af5bbba2
Oleg <oleg@unstoppabledomains.com>
2024-05-03 11:54:50 +0700

    Added other section and footer

--- unstoppable-ios-app/domains-manager-ios/Modules/Settings/SettingsView/SettingsView.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/Settings/SettingsView/SettingsView.swift
@@ -36,6 +36,8 @@ private extension SettingsView {
         ScrollView {
             //        profilesListView()
             moreSection()
+            othersSection()
+            footerView()
         }
         .padding()
     }
@@ -126,7 +128,6 @@ private extension SettingsView {
         .padding(EdgeInsets(4))
     }
     
-    
     func setTestnet(on isOn: Bool) {
         var settings = User.instance.getSettings()
         switch isOn {
@@ -142,6 +143,58 @@ private extension SettingsView {
         appContext.notificationsService.updateTokenSubscriptions()
     }
     
+}
+
+// MARK: - Other
+private extension SettingsView {
+    @ViewBuilder
+    func othersSection() -> some View {
+        otherItemsList()
+        .padding(.top, 16)
+    }
+    
+    @ViewBuilder
+    func otherItemsList() -> some View {
+        UDCollectionSectionBackgroundView {
+            VStack(alignment: .center, spacing: 0) {
+                ForEach(SettingsItems.allCases, id: \.self) { moreItem in
+                    listViewFor(otherItem: moreItem)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func listViewFor(otherItem: SettingsItems) -> some View {
+        UDCollectionListRowButton(content: {
+            UDListItemView(title: otherItem.title,
+                           titleColor: .foregroundAccent,
+                           imageType: .uiImage(otherItem.icon),
+                           imageStyle: .centred(offset: .init(8), foreground: .foregroundAccent, background: .clear, bordered: false))
+            .padding(.init(horizontal: 12, vertical: 4))
+        }, callback: {
+            UDVibration.buttonTap.vibrate()
+            logButtonPressedAnalyticEvents(button: otherItem.analyticsName)
+        })
+        .padding(EdgeInsets(4))
+    }
+}
+
+// MARK: - Footer
+private extension SettingsView {
+    @ViewBuilder
+    func footerView() -> some View {
+        VStack(spacing: 0) {
+            Text("You are Unstoppable!")
+                .foregroundStyle(Color.foregroundDefault)
+                .frame(height: 20)
+            Text(UserDefaults.buildVersion)
+                .foregroundStyle(Color.foregroundMuted)
+                .frame(height: 20)
+        }
+        .font(.currentFont(size: 14, weight: .medium))
+        .padding(.top, 24)
+    }
 }
 
 // MARK: - Entities
@@ -215,6 +268,56 @@ private extension SettingsView {
                 return .settingsTestnet
             }
         }
+    }
+    
+    enum SettingsItems: CaseIterable {
+        case rateUs, learn, twitter, support, legal
+        
+        var title: String {
+            switch self {
+            case .rateUs:
+                return String.Constants.rateUs.localized()
+            case .learn:
+                return String.Constants.settingsLearn.localized()
+            case .twitter:
+                return String.Constants.settingsFollowTwitter.localized()
+            case .support:
+                return String.Constants.settingsSupportNFeedback.localized()
+            case .legal:
+                return String.Constants.settingsLegal.localized()
+            }
+        }
+        
+        var icon: UIImage {
+            switch self {
+            case .rateUs:
+                return .iconStar24
+            case .learn:
+                return UIImage(named: "settingsIconLearn")!
+            case .twitter:
+                return UIImage(named: "settingsIconTwitter")!
+            case .support:
+                return UIImage(named: "settingsIconFeedback")!
+            case .legal:
+                return UIImage(named: "settingsIconLegal")!
+            }
+        }
+        
+        var analyticsName: Analytics.Button {
+            switch self {
+            case .rateUs:
+                return .settingsRateUs
+            case .learn:
+                return .settingsLearn
+            case .twitter:
+                return .settingsTwitter
+            case .support:
+                return .settingsSupport
+            case .legal:
+                return .settingsLegal
+            }
+        }
+
     }
 }
 
