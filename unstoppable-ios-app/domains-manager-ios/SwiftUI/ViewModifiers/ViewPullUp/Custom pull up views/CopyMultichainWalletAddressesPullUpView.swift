6f00d9ea83a904dc683ac2990f7d6d587133dd7c
Oleg <oleg@unstoppabledomains.com>
2024-05-07 10:47:36 +0700

    Prepare pull up

---
+++ unstoppable-ios-app/domains-manager-ios/SwiftUI/ViewModifiers/ViewPullUp/Custom pull up views/CopyMultichainWalletAddressesPullUpView.swift
@@ -0,0 +1,18 @@
+//
+//  CopyMultichainWalletAddressesPullUpView.swift
+//  domains-manager-ios
+//
+//  Created by Oleg Kuplin on 07.05.2024.
+//
+
+import SwiftUI
+
+struct CopyMultichainWalletAddressesPullUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CopyMultichainWalletAddressesPullUpView()
+}
