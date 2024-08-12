cbc0124f5e4eec3b9767aaceee6b065365d289f0
Oleg <oleg@unstoppabledomains.com>
2024-08-12 16:35:53 +0300

    Added confetti modifier

---
+++ unstoppable-ios-app/domains-manager-ios/Modules/PurchaseDomains/Purchased/PurchaseDomainsCompletedView.swift
@@ -0,0 +1,18 @@
+//
+//  PurchaseDomainsCompletedView.swift
+//  domains-manager-ios
+//
+//  Created by Oleg Kuplin on 12.08.2024.
+//
+
+import SwiftUI
+
+struct PurchaseDomainsCompletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PurchaseDomainsCompletedView()
+}
