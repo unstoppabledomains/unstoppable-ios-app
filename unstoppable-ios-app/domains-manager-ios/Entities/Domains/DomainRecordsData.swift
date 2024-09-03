feb2e5d6205a114f65f005474dff9bbb9c4199b6
Oleg <oleg@unstoppabledomains.com>
2024-09-03 16:04:20 +0300

    Fixed crypto records parsing to remove duplicates

--- unstoppable-ios-app/domains-manager-ios/Entities/Domains/DomainRecordsData.swift
+++ unstoppable-ios-app/domains-manager-ios/Entities/Domains/DomainRecordsData.swift
@@ -15,19 +15,27 @@ struct DomainRecordsData: Equatable, Codable {
     static let empty = DomainRecordsData(from: [:], coinRecords: [], resolver: nil)
      
     init(from recordsDict: [String: String], coinRecords: [CoinRecord], resolver: String?) {
-        let cryptoRecords: [CryptoRecord] = recordsDict.compactMap { dictElement in
-            let expandedTicker = dictElement.key
-            guard let coinRecord = coinRecords.first(where: { $0.isMatching(recordKey: expandedTicker) }) else {
-                Debugger.printWarning("Ignored record with key: \(expandedTicker)")
-                return nil
-            }
-            return CryptoRecord(coin: coinRecord,
-                                address: dictElement.value)
-        }
+        let cryptoRecords: [CryptoRecord] = recordsDict
+            .compactMap { Self.transformToCryptoRecord(dictElement: $0, in: coinRecords) }
+            .filter({ !$0.address.isEmpty })
         
-        // Remove duplicates after mapping old and new record structures 
-        self.records = Set(cryptoRecords).filter({ !$0.address.isEmpty })
+        /// Removes duplicates after merging
+        let groupedRecords = [CoinRecord : [CryptoRecord]].init(grouping: cryptoRecords, by: { $0.coin })
+        let records: [CryptoRecord] = groupedRecords.reduce([CryptoRecord](), { $0 + [$1.value.first!] })
+        
+        self.records = records
         self.resolver = resolver
         self.ipfsRedirectUrl = recordsDict[NetworkService.ipfsRedirectKey]
+    }
+    
+    private static func transformToCryptoRecord(dictElement: [String : String].Element, 
+                                                in coinRecords: [CoinRecord]) -> CryptoRecord? {
+        let expandedTicker = dictElement.key
+        guard let coinRecord = coinRecords.first(where: { $0.isMatching(recordKey: expandedTicker) }) else {
+            Debugger.printWarning("Ignored record with key: \(expandedTicker)")
+            return nil
+        }
+        return CryptoRecord(coin: coinRecord,
+                            address: dictElement.value)
     }
 }
