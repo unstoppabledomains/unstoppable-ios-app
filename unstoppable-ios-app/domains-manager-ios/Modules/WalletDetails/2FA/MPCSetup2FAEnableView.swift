6d587716514490a37560fb064a58541b18ed00c2
Oleg <oleg@unstoppabledomains.com>
2024-10-28 11:29:36 +0200

    Added qr code and continue button

--- unstoppable-ios-app/domains-manager-ios/Modules/WalletDetails/2FA/MPCSetup2FAEnableView.swift
+++ unstoppable-ios-app/domains-manager-ios/Modules/WalletDetails/2FA/MPCSetup2FAEnableView.swift
@@ -10,38 +10,66 @@ import SwiftUI
 struct MPCSetup2FAEnableView: View, ViewAnalyticsLogger {
 
     @Environment(\.mpcWalletsService) private var mpcWalletsService
+    @Environment(\.imageLoadingService) private var imageLoadingService
 
     var analyticsName: Analytics.ViewName { .setup2FAEnable }
     
     let wallet: WalletEntity
     let mpcMetadata: MPCWalletMetadata
     @State private var secret: String? = nil
-    
+    @State private var qrCodeImage: UIImage? = nil
     @State private var error: Error? = nil
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 32) {
-                headerView()
-                secretDetailsView()
+        ZStack(alignment: .bottom) {
+            ScrollView {
+                VStack(spacing: 32) {
+                    headerView()
+                    secretDetailsView()
+                }
             }
-            .padding(.horizontal, 16)
+            continueButtonView()
         }
+        .padding(.horizontal, 16)
         .background(Color.backgroundDefault)
         .onAppear(perform: onAppear)
+        .animation(.default, value: UUID())
         .displayError($error)
     }
 }
 
 private extension MPCSetup2FAEnableView {
     func onAppear() {
+        loadSecret()
+    }
+    
+    func loadSecret() {
         Task {
             do {
-//                secret = try await mpcWalletsService.requestOTPToEnable2FA(for: mpcMetadata)
+//                let secret = try await mpcWalletsService.requestOTPToEnable2FA(for: mpcMetadata)
                 await Task.sleep(seconds: 0.5)
-                secret = "GYZDOMRUMEZTANJSGZTDMOSDFSDVSDC"
+                let secret = "GYZDOMRUMEZTANJSGZTDMOSDFSDVSDC"
+                self.secret = secret
+                
+                loadQRCodeFor(secret: secret)
             } catch {
                 self.error = error
+            }
+        }
+    }
+
+    func loadQRCodeFor(secret: String) {
+        Task {
+            // Create Google Authenticator URL
+            let issuer = "Unstoppable"
+            let accountName = wallet.displayName
+            let urlString = "otpauth://totp/\(issuer):\(accountName)?secret=\(secret)&issuer=\(issuer)"
+            
+            if let encodedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+           let url = URL(string: encodedString) {
+            qrCodeImage = await imageLoadingService.loadImage(from: .qrCode(url: url,
+                                                                          options: []),
+                                                            downsampleDescription: nil)
             }
         }
     }
@@ -66,6 +94,7 @@ private extension MPCSetup2FAEnableView {
             VStack(spacing: 24) {
                 copySecretView(secret)
                 orScanSeparatorView()
+                qrCodeView()
             }
         } else {
             loadingSecretView()
@@ -115,6 +144,29 @@ private extension MPCSetup2FAEnableView {
                 .layoutPriority(2)
             HomeExploreSeparatorView()
                 .layoutPriority(1)
+        }
+    }
+
+    @ViewBuilder
+    func qrCodeView() -> some View {
+        if let qrCodeImage = qrCodeImage {
+            Image(uiImage: qrCodeImage)
+                .resizable()
+                .scaledToFit()
+                .squareFrame(200)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+        } else {
+            ProgressView()
+        }
+    }
+
+    @ViewBuilder
+    func continueButtonView() -> some View {
+        if secret != nil {
+            UDButtonView(text: String.Constants.continue.localized(),
+                         style: .large(.raisedPrimary), callback: {
+                logButtonPressedAnalyticEvents(button: .continue)
+            })
         }
     }
 }
